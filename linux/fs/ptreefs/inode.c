0a8a1044a58630c40e2af2a1f7f39542d3eac47f
Cunjun Wang <duckwcj@gmail.com>
2019-12-01 12:48:37 -0500

    add create file and create dir

--- linux/fs/ptreefs/inode.c
+++ linux/fs/ptreefs/inode.c
@@ -18,6 +18,7 @@ const struct super_operations ptreefs_super_operations = {
 	.drop_inode     = generic_delete_inode,
 };
 
+// TODO:
 static int ptreefs_dir_open(struct inode *inode, struct file *file) {
 	return 0;
 }
@@ -46,7 +47,8 @@ static int ptreefs_fill_super(struct super_block *sb, void *data, int silent)
 	if (!inode)
 		goto fail;
 	inode->i_ino = 1;
-	inode->i_mtime = inode->i_atime = inode->i_ctime = current_time(inode);
+	inode->i_mtime = inode->i_atime = 
+		inode->i_ctime = current_time(inode);
 	inode->i_mode = S_IFDIR | S_IRUGO | S_IXUGO | S_IWUSR;
 	inode->i_op = &simple_dir_inode_operations;
 	inode->i_fop = &ptreefs_dir_operations;
@@ -90,17 +92,17 @@ static struct file_system_type ptree_fs_type = {
 // 	.fsync			= noop_fsync,
 // };
 
-static ssize_t ptreefs_read_file(struct file *file, char __user *buf,
-	size_t count, loff_t *ppos)
+static ssize_t ptreefs_read_file(struct file *file, 
+	char __user *buf, size_t count, loff_t *ppos)
 {
 	return 0;
-};
+}
 
-static ssize_t ptreefs_write_file(struct file *file, const char __user *buf,
-	size_t count, loff_t *ppos)
+static ssize_t ptreefs_write_file(struct file *file, 
+	const char __user *buf, size_t count, loff_t *ppos)
 {
 	return count;
-};
+}
 
 const struct file_operations ptreefs_file_operations = {
 	.open		= simple_open,
@@ -108,16 +110,81 @@ const struct file_operations ptreefs_file_operations = {
 	.write		= ptreefs_write_file,
 };
 
-struct dentry *ptreefs_create_file(const char *name, umode_t mode,
-				   struct dentry *parent, void *data,
-				   const struct file_operations *fops)
+static struct inode *ptreefs_make_inode(struct super_block *sb, int mode)
 {
-	return NULL;
+	struct inode *inode;
+
+	inode = new_inode(sb);
+
+	if (!inode)
+		return NULL;
+
+	inode->i_ino = get_next_ino();
+	inode->i_mtime = inode->i_atime = 
+		inode->i_ctime = current_time(inode);
+	inode->i_mode = mode;
+	// inode->i_blkbits = PAGE_CACHE_SIZE;
+	// inode->i_blocks = 0;
+
+	return inode;
+};
+
+struct dentry *ptreefs_create_file(struct super_block *sb,
+	struct dentry *dir, const char* name)
+{
+	struct inode *inode;
+	struct dentry *dentry;
+	struct qstr qname;
+
+	qname.name = name;
+	qname.len = strlen(name);
+	// no salt value
+	qname.hash = full_name_hash(NULL, name, qname.len);
+
+	dentry = d_alloc(dir, &qname);
+	if (!dentry)
+		return NULL;
+
+	// permission 0555: read allowed, execute allowed, write prohibiteds
+	inode = ptreefs_make_inode(sb, S_IFREG | 0555);
+	if (!inode) {
+		dput(dentry); // release a dentry
+		return NULL;
+	}
+	inode->i_fop = &ptreefs_file_operations;
+
+	d_add(dentry, inode);
+
+	return dentry;
 }
 
-struct dentry *ptreefs_create_dir(const char *name, struct dentry *parent)
+struct dentry *ptreefs_create_dir(struct super_block *sb, 
+	const char *name, struct dentry *parent)
 {
-	return NULL;
+	struct dentry *dentry;
+	struct inode *inode;
+	struct qstr qname;
+
+	qname.name = name;
+	qname.len = strlen(name);
+	// no salt value
+	qname.hash = full_name_hash(NULL, name, qname.len);
+
+	dentry = d_alloc(parent, &qname);
+	if (!dentry)
+		return NULL;
+
+	// permission 0555: read allowed, execute allowed, write prohibiteds
+	inode = ptreefs_make_inode(sb, S_IFDIR | 0555);
+	if (!inode) {
+		dput(dentry);
+		return NULL;
+	}
+	inode->i_op = &simple_dir_inode_operations;
+	inode->i_fop = &simple_dir_operations;
+
+	d_add(dentry, inode);
+	return dentry;
 }
 
 static int __ptreefs_remove(struct dentry *dentry, struct dentry *parent)
